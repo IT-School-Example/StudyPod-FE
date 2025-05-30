ef6c46faff0007b5e9a60e54995168f4ec0cb6bb
ryu <yhd060210@gmail.com>
2025-05-30 13:16:28 +0900

    🎨 manage 부분(소개 및 공지 작성 페이지)
    다른 부분들은 개발 진행 중이라 커밋 안함

---
+++ user/app/(pages)/studyGroup/components/(manage)/introduce.jsx
@@ -0,0 +1,8 @@
+
+export default function Introduce({study}) {
+  return (
+    <div>
+      <h1>소개글 작성 페이지입니다!</h1>
+    </div>
+  );
+}
---
+++ user/app/(pages)/studyGroup/components/(manage)/notice.jsx
@@ -0,0 +1,8 @@
+
+export default function Notice({study}) {
+  return (
+    <div>
+      <h1>공지 작성 페이지입니다!</h1>
+    </div>
+  );
+}
