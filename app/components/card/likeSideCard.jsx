c2b4046cce3d41f30512c7f46dbbef786ffbb107
ryu <yhd060210@gmail.com>
2025-06-09 15:41:46 +0900

    🔨 좋아요 누른 스터디 목록

---
+++ app/components/card/likeSideCard.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import { FaHeart } from "react-icons/fa6";
+
+export default function LikeSideCard({ tag, content, leader, like, detail, url }) {
+  return (
+    <Link href={`/studyGroup/${detail}${url ?? ""}`}>
+      <div className="p-4 w-full border-2 border-black rounded-xl hover:border-blue-300 hover:scale-105 transition">
+        <div className="mb-3">
+          <div className="inline-block px-3 py-1 text-sm bg-[#FBEDD7] rounded-xl mb-2">
+            {tag}
+          </div>
+          <h2 className="font-bold text-lg text-black">{content}</h2>
+        </div>
+        <div className="border-t border-gray-300 pt-2 flex justify-between items-center text-sm text-black">
+          <span>리더: {leader}</span>
+          <span className="flex items-center gap-1">
+            <FaHeart className="text-red-500" />
+            {like}
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+}
