2b6edcb516a59e722a42ac2b20e203545cfcb25f
ryu <yhd060210@gmail.com>
2025-06-10 10:53:24 +0900

    📝 dev환경과 베포환경에서 적용하는 url 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: 'https://studypod.click/api',
+    NEXT_PUBLIC_API_URL:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:8080/api"
+        : "https://studypod.click/api", // 배포 환경
   },
   reactStrictMode: true,
   swcMinify: true,
