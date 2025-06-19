f5d4a6a21be2758d5167964933f877d305f62ca7
ryu <yhd060210@gmail.com>
2025-06-19 16:12:44 +0900

    🔨 user context 적용

--- app/mypage/applied/page.jsx
+++ app/mypage/applied/page.jsx
@@ -3,27 +3,16 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import IntroSideCard from "@/components/card/introSideCard";
+import { useUser } from "@/context/UserContext";
 
 export default function Applied() {
-  const [userId, setUserId] = useState(null);
+  const { user } = useUser();
   const [appliedStudies, setAppliedStudies] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
     const fetchAppliedStudies = async () => {
       try {
-        // 로그인 유저 정보 조회
-        const userRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/me`, {
-          method: "GET",
-          credentials: "include",
-        });
-
-        if (!userRes.ok) throw new Error("회원 정보를 불러올 수 없습니다.");
-
-        const user = await userRes.json();
-        setUserId(user.id);
-
-        // 신청한 스터디 목록 조회 (PENDING 상태)
         const res = await fetch(
           `${process.env.NEXT_PUBLIC_API_URL}/study-groups/user/${user.id}/enrolled-groups?enrollmentStatus=PENDING`,
           {
@@ -38,7 +27,6 @@ export default function Applied() {
         setAppliedStudies(json.data || []);
       } catch (err) {
         console.error("데이터 조회 오류:", err);
-        alert("로그인이 필요합니다.");
         router.push("/login");
       }
     };
