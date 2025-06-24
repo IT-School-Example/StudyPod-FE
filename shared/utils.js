f2fee305a1c1eb5358b9f7512b6e09d7e947f061
ryu <yhd060210@gmail.com>
2025-06-25 08:31:19 +0900

    🔨 스터디 주제 id로 이름 조회

--- shared/utils.js
+++ shared/utils.js
@@ -1,6 +1,11 @@
-import { sidoOptions } from "./constants";
+import { sidoOptions, subjectOptions } from "./constants";
 
 export const getSidoName = (sidoCd) => {
   const match = sidoOptions.find((sido) => sido.sidoCd === sidoCd);
   return match ? match.sidoNm : "알 수 없음";
+};
+
+export const getSubjectNm = (id) => {
+  const match = sidoOptions.find((subject) => subject.id === id);
+  return match ? match.value : "알 수 없음";
 };
