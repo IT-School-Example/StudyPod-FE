6cc08d56f77ad524e1bf44ae45bffb75b7b1a11a
ryu <yhd060210@gmail.com>
2025-06-25 08:28:03 +0900

    🔨 tailwind로 수정

--- components/chat/chatRoomList.jsx
+++ components/chat/chatRoomList.jsx
@@ -1,24 +1,24 @@
+"use client";
 import React from "react";
 
 export default function ChatRoomList({ rooms, currentRoomId, onSelectRoom }) {
   return (
-    <div>
-      <h3>채팅방 목록</h3>
-      <ul>
-        {rooms.length === 0 && <li>채팅방이 없습니다. </li>}
+    <div className="w-full">
+      <h3 className="text-lg font-semibold mb-2">채팅방 목록</h3>
+      <ul className="space-y-1">
+        {rooms.length === 0 && (
+          <li className="text-gray-500">채팅방이 없습니다.</li>
+        )}
         {rooms.map((room) => (
           <li
             key={room.id}
             onClick={() => onSelectRoom(room.id)}
-            style={{
-              cursor: "pointer",
-              fontweight: currentRoomId === room.id ? "bold" : "normal",
-              backgroundColor:
-                currentRoomId === room.id ? "#d3d3d3" : "transparent",
-              padding: "5px",
-              margin: "3px 0",
-              userSelect: "none",
-            }}
+            className={`cursor-pointer px-4 py-2 rounded-md transition
+              ${
+                currentRoomId === room.id
+                  ? "bg-gray-300 font-bold"
+                  : "hover:bg-gray-100"
+              }`}
           >
             {room.name} [{room.type === "DIRECT" ? "1:1" : "그룹"}]
           </li>
